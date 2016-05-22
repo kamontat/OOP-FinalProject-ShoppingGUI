abd1e0b045c3a54ab12eba359f54b09938bd2d06
kamontat <kamontat_c@hotmail.com>
2016-05-22 18:51:18 +0700

    create new gui form

--- src/gui1/ShoppingPage.java
+++ src/gui1/ShoppingPage.java
@@ -1,8 +1,15 @@
 package gui1;
 
+import javax.swing.*;
+
 /**
  * @author kamontat
  * @since 22/5/59 - 17:34
  */
 public class ShoppingPage {
+	private JPanel panel1;
+	private JTable table1;
+	private JButton mainPageButton;
+	private JButton basketButton;
+	private JButton paymentButton;
 }
