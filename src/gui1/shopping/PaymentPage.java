c1829a16d48c1db2385c52d4cb24471892f335bc
kamontat <kamontat_c@hotmail.com>
2016-06-01 19:21:33 +0700

    add name for container

--- src/gui1/shopping/PaymentPage.java
+++ src/gui1/shopping/PaymentPage.java
@@ -7,11 +7,15 @@ import javax.swing.*;
  * @since 1/6/59 - 16:36
  */
 public class PaymentPage {
-	private JTable table1;
+	private JTable table;
 	private JButton buyButton;
-	private JButton mainPageButton;
-	private JButton shoppingPageButton;
-	private JComboBox comboBox1;
+	private JButton mainButton;
+	private JButton shoppingButton;
+	private JComboBox comboBox;
 	private JTextField priceLabel;
 	private JTextField weightLabel;
+	private JLabel discountLabel;
+	private JLabel memberLabel;
+	private JLabel customerLabel;
+	private JLabel finalPriceLabel;
 }
