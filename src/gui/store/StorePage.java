1d9f8f460778cc37e35d6a714e9880d27ed36252
kamontat <kamontat_c@hotmail.com>
2016-11-17 11:11:51 +0700

    add , into store page

--- src/gui/store/StorePage.java
+++ src/gui/store/StorePage.java
@@ -22,7 +22,7 @@ import java.io.File;
  */
 public class StorePage extends JFrame implements Table, ButtonFactory {
 	private Store store = Store.getInstance();
-
+	
 	private JPanel panel;
 	private JTextField searchField;
 	private JTable table;
@@ -33,29 +33,29 @@ public class StorePage extends JFrame implements Table, ButtonFactory {
 	private JLabel expenseLabel;
 	private JLabel profitLabel;
 	private JButton checkButton;
-
+	
 	private ProductModel model;
-
+	
 	public StorePage() {
 		super("Store Page");
 		setContentPane(panel);
 		model = new ProductModel(store.getAllProduct(true), new String[]{"ID", "Name", "Type", "Material", "Size", "Weight", "In Stock", "Restock", "Price", "Buying Price"});
-
+		
 		settingTable();
-
+		
 		// assign button
 		toMain(this, mainButton);
 		restockButton.addActionListener(e -> restock());
 		history();
 		check();
-
+		
 		updateLabel(store.getRevenue(), store.getExpense());
 	}
-
+	
 	private void settingTable() {
 		table.setModel(model);
 		settingTable(table, new int[]{50, 190, 75, 180, 65, 65, 75, 75, 80, 100});
-
+		
 		table.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -66,18 +66,18 @@ public class StorePage extends JFrame implements Table, ButtonFactory {
 				}
 			}
 		});
-
+		
 		TableRowSorter<TableModel> rowSorter = new TableRowSorter<>(table.getModel());
 		table.setRowSorter(rowSorter);
 		searching(searchField, rowSorter);
 	}
-
+	
 	private void restock() {
 		int row = table.getSelectedRow();
-
+		
 		if (checkValid(row)) {
 			ProductExt product = getProductAt(row);
-
+			
 			if (product.isRestock()) {
 				store.addExpense(product.restockProductExt());
 				store.setRestockProduct(false);
@@ -94,7 +94,7 @@ public class StorePage extends JFrame implements Table, ButtonFactory {
 			}
 		}
 	}
-
+	
 	private void history() {
 		historyButton.addActionListener(e -> {
 			int row = table.getSelectedRow();
@@ -105,12 +105,12 @@ public class StorePage extends JFrame implements Table, ButtonFactory {
 			resetSelection(table);
 		});
 	}
-
+	
 	private void check() {
 		checkButton.addActionListener(evt -> {
 			MainPage.reWriteStoreInfo();
 			MainPage.reWriteProductInfo();
-
+			
 			int status = JOptionPane.showConfirmDialog(null, "Did store payment is correct?", "Checking", JOptionPane.YES_NO_OPTION);
 			// mean no
 			if (status == 1) {
@@ -131,22 +131,22 @@ public class StorePage extends JFrame implements Table, ButtonFactory {
 			}
 		});
 	}
-
+	
 	private ProductExt getProductAt(int row) {
 		String productID = String.valueOf(table.getValueAt(row, 0));
 		return store.searchProductID(productID);
 	}
-
+	
 	private void updateLabel(double revenue, double expense) {
-		revenueLabel.setText("Revenue: " + revenue);
-		expenseLabel.setText("Expense: " + expense);
-		profitLabel.setText("Profit: " + (revenue - expense));
+		revenueLabel.setText(String.format("Revenue: %,f", revenue));
+		expenseLabel.setText(String.format("Expense: %,f", expense));
+		profitLabel.setText(String.format("Profit: %,f", (revenue - expense)));
 	}
-
+	
 	private void updateTable(Object newValue, int row, int column) {
 		table.setValueAt(newValue, row, column);
 	}
-
+	
 	public void run(Point point) {
 		setMinimumSize(new Dimension(970, 500));
 		pack();
