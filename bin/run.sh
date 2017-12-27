32ca7558ac9090466083cc4817d4ca49ed673d75
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2017-12-27 13:32:57 +0700

    :package: Add compile file

---
+++ bin/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# set -x #DEBUG - Display commands and their arguments as they are executed.
+# set -v #VERBOSE - Display shell input lines as they are read.
+# set -n #EVALUATE - Check syntax of the script but don't execute.
+
+cd "$(dirname "$0")"
+
+echo "run.."
+java code.main.Application
